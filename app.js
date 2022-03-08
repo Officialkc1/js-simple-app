00e3bf1ed6ed1a16d745e3ce499a0695b320782d
officialkc1 <kiherome@gamil.com>
2022-03-08 17:30:20 +0100

    warning colors updated

--- app.js
+++ app.js
@@ -24,13 +24,21 @@ function updateRemainingChars(event) {
     // console.log(remainingCharsElement.textContent);
 
     // if (enteredTextLength >= 50) {
+    //     remainingCharsElement.classList.add('warning');
     //     productNameInput.className = 'warning'
     // } else {
+    //     remainingCharsElement.classList.remove('warning')
     //     productNameInput.classList.remove('warning')
     // }
-    if (remainingChars <= 10) {
+
+    if (remainingChars === 0) {
+        remainingCharsElement.classList.add('error');
+        productNameInput.classList.add('error');
+    } else if (remainingChars <= 10) {
         remainingCharsElement.classList.add('warning');
         productNameInput.classList.add('warning');
+        remainingCharsElement.classList.remove('error');
+        productNameInput.classList.remove('error')
     } else {
         remainingCharsElement.classList.remove('warning');
         productNameInput.classList.remove('warning')
